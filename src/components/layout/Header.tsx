26194d9630c08595cc901b6f97ff0f6fef908ed4
Henrique_silva <henriquesilva9648@outlook.com>
2024-01-12 11:29:13 -0300

    Header implemented.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,21 @@
+import "./header.css";
+
 export default function Header() {
     return (
         <header>
-            <div>
+            <div className="header flex-row-around">
                 <div>
-                    <h1>HB Enquetes</h1>
+                    <a href="/" className="clean-link-no-hover" title="Voltar para página inicial">
+                        <h1>HB Enquetes</h1>
+                    </a>
+                </div>
+
+                <div className="links">
+                    <div>
+                        <a href="/register" className="clean-link" title="Criar nova enquete">
+                            Criar nova enquete
+                        </a>
+                    </div>
                 </div>
             </div>
         </header>
