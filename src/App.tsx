4844c686bda6d1aeec9024c28ba346006737d429
Henrique_silva <henriquesilva9648@outlook.com>
2024-01-12 15:30:24 -0300

    Added react-toastify.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import RoutesControl from './Routes';
+import { ToastContainer } from 'react-toastify';
+
+import "react-toastify/ReactToastify.min.css";
 
 export default function App() {
   return (
     <BrowserRouter>
       <RoutesControl />
+      <ToastContainer />
     </BrowserRouter>
   );
 }
