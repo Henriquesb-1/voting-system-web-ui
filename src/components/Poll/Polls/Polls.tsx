638608ccb098c406662b3d3a42c6063c4b3dd699
Henrique_silva <henriquesilva9648@outlook.com>
2024-01-15 15:56:06 -0300

    Added no polls warning.

--- src/components/Poll/Polls/Polls.tsx
+++ src/components/Poll/Polls/Polls.tsx
@@ -14,51 +14,62 @@ export default function Polls({ currentPage }: PollsProps) {
     const [polls, setPolls] = useState<PollModel[]>([]);
     const [pages, setPages] = useState<number>(1);
 
-    useEffect(() => {
-        APICall.get(`/poll?page=${currentPage}`)
-            .then(resp => {
-                const { data } = resp;
-                setPolls(data.data);
-                setPages(data.pages)
-            });
-    }, [])
+    // useEffect(() => {
+    //     APICall.get(`/poll?page=${currentPage}`)
+    //         .then(resp => {
+    //             const { data } = resp;
+    //             setPolls(data.data);
+    //             setPages(data.pages)
+    //         });
+    // }, [])
 
     const goToPoll = (link: string) => window.location.replace(`/poll/${link}`);
-    
+
 
     function renderPolls() {
-        return polls.map((poll, index) => {
+        if (polls.length > 0) {
+            return polls.map((poll, index) => {
+                return (
+                    <div className="poll flex-column-between margin-y" key={`poll-item-${poll._id}`}>
+                        <div className="poll-title">
+                            <h3>{poll._title}</h3>
+                        </div>
+
+                        <div className="poll-start-date">
+                            <span>
+                                {poll._status === PollStatus.EXPIRED || poll._status === PollStatus.IN_CURSE ? `Iniciou em ${poll._startDate}` : `Inicia em ${poll._startDate}`}
+                            </span>
+                        </div>
+
+                        <div className="poll-end-date">
+                            <span>
+                                {poll._status === PollStatus.EXPIRED ? `Terminou em ${poll._endDate}` : `Termina em ${poll._endDate}`}
+                            </span>
+                        </div>
+
+                        <div className="poll-link">
+                            <button style={{ color: "#000" }}
+                                onClick={e => goToPoll(poll._title)}
+                                disabled={poll._status === PollStatus.WILL_START}
+                                title={poll._status === PollStatus.WILL_START ? `Irá ficar disponivel em ${poll._startDate}` : `Acessar enquete`}
+                                className="clean-button"
+                            >
+                                Acessar enquete
+                            </button>
+                        </div>
+                    </div>
+                )
+            })
+        } else {
             return (
-                <div className="poll flex-column-between margin-y" key={`poll-item-${poll._id}`}>
-                    <div className="poll-title">
-                        <h3>{poll._title}</h3>
-                    </div>
-
-                    <div className="poll-start-date">
-                        <span>
-                            {poll._status === PollStatus.EXPIRED || poll._status === PollStatus.IN_CURSE ? `Iniciou em ${poll._startDate}` : `Inicia em ${poll._startDate}`}
-                        </span>
-                    </div>
-
-                    <div className="poll-end-date">
-                        <span>
-                            {poll._status === PollStatus.EXPIRED ? `Terminou em ${poll._endDate}` : `Termina em ${poll._endDate}`}
-                        </span>
-                    </div>
-
-                    <div className="poll-link">
-                        <button style={{ color: "#000" }}
-                            onClick={e => goToPoll(poll._title)}
-                            disabled={poll._status === PollStatus.WILL_START}
-                            title={poll._status === PollStatus.WILL_START ? `Irá ficar disponivel em ${poll._startDate}` : `Acessar enquete`}
-                            className="clean-button"
-                        >
-                            Acessar enquete
-                        </button>
-                    </div>
+                <div className="flex-row-center">
+                    <h3>
+                        Nenhuma enquete criada. <br />
+                        <a href="/register" className="clean-link" style={{color: "#000"}}>Criar uma</a>
+                    </h3>
                 </div>
             )
-        })
+        }
     }
 
     return (
