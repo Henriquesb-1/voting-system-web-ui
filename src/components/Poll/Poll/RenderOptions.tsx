721153ac2fe52afed06da603cfc9d34867023e5a
Henrique_silva <henriquesilva9648@outlook.com>
2024-01-15 11:19:12 -0300

    Fixed some issues.

--- src/components/Poll/Poll/RenderOptions.tsx
+++ src/components/Poll/Poll/RenderOptions.tsx
@@ -6,6 +6,7 @@ import FeedBack from "../../../utils/Feedback";
 import "./renderOptions.css";
 
 interface RenderOptionsProps {
+    pollId: number;
     options: Option[];
     setOptions: Dispatch<SetStateAction<Option[]>>;
     hasExpired: boolean;
@@ -13,14 +14,14 @@ interface RenderOptionsProps {
 
 const blankOption: Option = { id: 0, content: "", voteCount: 0, pollId: 0 };
 
-export default function RenderOptions({ options, setOptions, hasExpired }: RenderOptionsProps) {
+export default function RenderOptions({ pollId, options, setOptions, hasExpired }: RenderOptionsProps) {
     const [optionVoted, setOptionVoted] = useState<Option>(blankOption);
     const [hasAlreadyVoted, setHasAlreadyVoted] = useState<boolean>(false);
 
     useEffect(() => {
         const lastOptionVoted = localStorage.getItem("last_vote") || "";
 
-        if(lastOptionVoted) {
+        if (lastOptionVoted) {
             setOptionVoted(JSON.parse(lastOptionVoted));
             setHasAlreadyVoted(true);
         }
@@ -34,18 +35,18 @@ export default function RenderOptions({ options, setOptions, hasExpired }: Rende
     function handleVote() {
         const voteCount = hasAlreadyVoted ? optionVoted.voteCount - 1 : optionVoted.voteCount + 1;
 
-        APICall.put(`/options`, {id: optionVoted.id, content: optionVoted.content, voteCount})
+        APICall.put(`/options`, { id: optionVoted.id, content: optionVoted.content, voteCount, poll: { id: pollId }, willUpdateVoteCount: true })
             .then(resp => {
-                const updatedOptionVoted = {id: optionVoted.id, content: optionVoted.content, voteCount, pollId: optionVoted.pollId}
+                const updatedOptionVoted = { id: optionVoted.id, content: optionVoted.content, voteCount, pollId: optionVoted.pollId }
 
                 setOptions(options.map(option => {
-                    if(option.id === updatedOptionVoted.id) option.voteCount = voteCount;
+                    if (option.id === updatedOptionVoted.id) option.voteCount = voteCount;
                     return option;
                 }))
 
-                if(hasAlreadyVoted) {
+                if (hasAlreadyVoted) {
                     selectOption(blankOption);
-                    setOptionVoted({id: optionVoted.id, content: optionVoted.content, voteCount, pollId: optionVoted.pollId});
+                    setOptionVoted({ id: optionVoted.id, content: optionVoted.content, voteCount, pollId: optionVoted.pollId });
                     localStorage.removeItem("last_vote");
                     setHasAlreadyVoted(false);
                     FeedBack.success("Voto removido");
@@ -62,7 +63,7 @@ export default function RenderOptions({ options, setOptions, hasExpired }: Rende
     function renderOptions() {
         return options.map(option => {
             return (
-                <li onClick={e => hasExpired  || hasAlreadyVoted ? false : selectOption(option)} key={`option-item-${option.content}`} className="poll-vote-option margin-y" style={{ backgroundColor: `${optionVoted.id === option.id ? "#f00" : ""}` }}>
+                <li onClick={e => hasExpired || hasAlreadyVoted ? false : selectOption(option)} key={`option-item-${option.content}`} className="poll-vote-option margin-y" style={{ backgroundColor: `${optionVoted.id === option.id ? "#f00" : ""}` }}>
                     {option.content}
 
                     <span>{option.voteCount} votos</span>
